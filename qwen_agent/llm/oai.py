e3bfc9fa5eca67e899b51b8ffc3d2268b7ae925f
Xu Song <xusong.vip@gmail.com>
2025-08-20 21:02:08 +0800

    Update oai.py

--- qwen_agent/llm/oai.py
+++ qwen_agent/llm/oai.py
@@ -76,7 +76,7 @@ class TextChatAtOAI(BaseFnCallModel):
                 if 'request_timeout' in kwargs:
                     kwargs['timeout'] = kwargs.pop('request_timeout')
                 logger.info(f"calling chat_completion service={api_kwargs}, args={args}, kwargs={kwargs}")
-                logger.info(f"calling chat_completion kwargs={json.dumps(kwargs, ensure_ascii=False, indent=2)")
+                logger.info(f"calling chat_completion kwargs={json.dumps(kwargs, ensure_ascii=False, indent=2)}")
                 client = openai.OpenAI(**api_kwargs)
                 return client.chat.completions.create(*args, **kwargs)
 
@@ -91,7 +91,7 @@ class TextChatAtOAI(BaseFnCallModel):
                 if 'request_timeout' in kwargs:
                     kwargs['timeout'] = kwargs.pop('request_timeout')
                 logger.info(f"calling chat_completion service={api_kwargs}, args={args}, kwargs={kwargs}")
-                logger.info(f"calling chat_completion kwargs={json.dumps(kwargs, ensure_ascii=False, indent=2)")
+                logger.info(f"calling chat_completion kwargs={json.dumps(kwargs, ensure_ascii=False, indent=2)}")
                 client = openai.OpenAI(**api_kwargs)
                 return client.completions.create(*args, **kwargs)
 
